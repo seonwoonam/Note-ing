08def8d1a0e7bd82a10d7ea0fe84e180c3e3780c
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2023-11-10 14:08:33 +0900

    [FEAT] Make CalendarView and TodoListView

---
+++ Note-ing/Note-ing/CalendarView.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarView.swift
+//  Note-ing
+//
+//  Created by 윤지성 on 2023/11/10.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text("calendar view")
+    }
+}
+
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+    }
+}
---
+++ Note-ing/Note-ing/TodoListView.swift
@@ -0,0 +1,20 @@
+//
+//  TodoListView.swift
+//  Note-ing
+//
+//  Created by 윤지성 on 2023/11/10.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        Text("to do list view!")
+    }
+}
+
+struct TodoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoListView()
+    }
+}
